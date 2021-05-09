52e7fede5c29e95a313a8ab8aff06e5d252d001a
BrianH <78457490+sheepesq@users.noreply.github.com>
2021-05-08 22:05:12 -0400

    commit

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -20,13 +20,47 @@ streets.addTo(map);
 //  Add a marker to the map for Los Angeles, California.
 //let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
-// add a circle to the map
+/* // add a circle to the map
 L.circle([34.0522, -118.2437], {
     color: 'black',
     fillColor: 'yellow',
     fillOpacity: 0.3,
     radius: 3000
- }).addTo(map);
+ }).addTo(map); */
+
+
+ // An array containing each city's location, state, and population.
+let cities = [{
+    location: [40.7128, -74.0059],
+    city: "New York City",
+    state: "NY",
+    population: 8398748
+  },
+  {
+    location: [41.8781, -87.6298],
+    city: "Chicago",
+    state: "IL",
+    population: 2705994
+  },
+  {
+    location: [29.7604, -95.3698],
+    city: "Houston",
+    state: "TX",
+    population: 2325502
+  },
+  {
+    location: [34.0522, -118.2437],
+    city: "Los Angeles",
+    state: "CA",
+    population: 3990456
+  },
+  {
+    location: [33.4484, -112.0740],
+    city: "Phoenix",
+    state: "AZ",
+    population: 1660272
+  }
+  ];
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
