e8b2330c23d6c206c2a17acd1e32b938303a27a5
BrianH <78457490+sheepesq@users.noreply.github.com>
2021-05-10 13:03:44 -0400

    commit

---
+++ mapping_multi_Geo/static/js/logic.js
@@ -0,0 +1,33 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with center and zoom level.
+let map = L.map('mapid').setView([30, 30], 2);
+
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
+
+/* **************************************** 13.5.3
+Having the tileLayer() method before accessing large datasets ensures that 
+the map gets loaded before the data is added to it. */
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/<GitHub_name>/Mapping_Earthquakes/main/majorAirports.json";
+
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJson(data).addTo(map);
+});
